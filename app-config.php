731ef761b3adb66804fa1bfa26d87ac381f49e3d
Sergey Cherdantsev <liverenemy@gmail.com>
2015-08-13 21:03:48 +0700

    Theme config

---
+++ app-config.php
@@ -0,0 +1,6 @@
+<?php
+define( 'UPLOADS', '../uploads' );
+define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content' );
+define( 'WP_PLUGIN_DIR', dirname(__FILE__) . '/wp-content/plugins' );
+define( 'WP_PLUGIN_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content/plugins' );
---
+++ readme.md
@@ -0,0 +1,5 @@
+<h1>Wordpress composer app</h1>
+<p>
+    Don't forget to include <pre>app-config.php</pre> in your <pre>wp-config.php</pre>
+    right after the ABSPATH constant is set.
+</p>
